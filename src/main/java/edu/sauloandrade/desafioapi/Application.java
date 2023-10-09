8bba22f0e6cd76446035d10653b2ab1a6b4b86f5
Saulo Andrade <sauloandradegames@gmail.com>
2023-10-08 22:34:43 -0300

    Bugfix CORS

--- src/main/java/edu/sauloandrade/desafioapi/Application.java
+++ src/main/java/edu/sauloandrade/desafioapi/Application.java
@@ -3,6 +3,10 @@ package edu.sauloandrade.desafioapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server Url")})
 @SpringBootApplication
 public class Application {
 
